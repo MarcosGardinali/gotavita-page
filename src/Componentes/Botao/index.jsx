e333da64bddeda1323a3478bf8432d2f58cbddae
MarcosGardinali <marcosgardinali21@outlook.com>
2023-12-17 16:07:30 -0300

    update

---
+++ src/Componentes/Botao/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import style from './botao.module.css';
+
+export default function Botao({ children, link, targetId }) {
+  const scrollToSection = (e) => {
+    e.preventDefault();
+
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
+    }
+
+    if (link) {
+      window.location.href = link;
+    }
+
+  };
+
+  return (
+    <a href={link} rel="nofollow" onClick={scrollToSection} className={style.botao}>
+      {children}
+    </a>
+  );
+}
